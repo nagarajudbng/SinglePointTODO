cc324d3f322f982cb3d74b9fa767cac3a53bf86d
nagarajudbng <nagaraj.dbng@gmail.com>
2024-05-07 01:35:59 +0530

    Test Case Successfully  PASSED

--- app/src/test/java/com/single/point/feature_taskcreate/data/local/TaskRepositoryImplTest.kt
+++ app/src/test/java/com/single/point/feature_taskcreate/data/local/TaskRepositoryImplTest.kt
@@ -1,32 +1,43 @@
 package com.single.point.feature_taskcreate.data.local
 
 import com.single.point.core.data.database.AppDatabase
+import com.single.point.core.data.database.Task
+import com.single.point.core.data.database.TaskDao
+import com.single.point.feature_taskcreate.domine.repository.TaskRepository
 import com.single.point.feature_taskcreate.data.TaskRepositoryImpl
 import kotlinx.coroutines.test.runBlockingTest
 import org.junit.Assert.assertEquals
 import org.junit.Before
 import org.junit.Test
+import org.mockito.InjectMocks
 import org.mockito.Mock
+import org.mockito.Mockito.`when`
 import org.mockito.MockitoAnnotations
 
 class TaskRepositoryImplTest {
 
-    private lateinit var repository: TaskRepository
 
     @Mock
-    private var appDatabase:AppDatabase
+    private lateinit var appDatabase:AppDatabase
+
+    @Mock
+    private lateinit var taskDao: TaskDao
+
+    @InjectMocks
+    private lateinit var repository: TaskRepositoryImpl
 
     @Before
     fun setup() {
         MockitoAnnotations.initMocks(this)
-        repository = TaskRepositoryImpl(appDatabase)
+//        repository = TaskRepositoryImpl(appDatabase)
     }
     @Test
     fun testAddTask()=runBlockingTest{
         var task = Task(title="Title",description="Description")
-        'when'(appDatabase.taskDao().insertTask(task)).thenReturn(1)
-        var  taskId = repository.addTask(task)
-        assertEquals(1,taskId)
-
+        var id:Long = 1L
+        `when` (appDatabase.taskDao).thenReturn(taskDao)
+        `when`(taskDao.insertTask(task)).thenReturn(id)
+        var  taskId = repository.insertTask(task)
+        assertEquals(id,taskId)
     }
 }
