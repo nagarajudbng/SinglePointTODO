68c54b7203805dd88f14687215719f12f76968f6
nagarajudbng <nagaraj.dbng@gmail.com>
2024-05-07 01:35:39 +0530

    Hilt Entry points added

---
+++ app/src/main/java/com/single/point/TODOApplication.kt
@@ -0,0 +1,9 @@
+package com.single.point
+
+import android.app.Application
+import dagger.hilt.android.HiltAndroidApp
+
+// Created by Nagaraju Deshetty on 07/05/24.
+@HiltAndroidApp
+class TODOApplication: Application() {
+}
