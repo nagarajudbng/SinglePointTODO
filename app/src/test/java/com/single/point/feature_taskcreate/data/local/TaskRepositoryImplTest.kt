51f558ff0ad3b37610b0d74e502c392bc254b42c
nagarajudbng <nagaraj.dbng@gmail.com>
2024-05-07 00:40:13 +0530

    Updated TestCase

--- app/src/test/java/com/single/point/feature_taskcreate/data/local/TaskRepositoryImplTest.kt
+++ app/src/test/java/com/single/point/feature_taskcreate/data/local/TaskRepositoryImplTest.kt
@@ -1,5 +1,7 @@
 package com.single.point.feature_taskcreate.data.local
 
+import com.single.point.core.data.database.AppDatabase
+import com.single.point.feature_taskcreate.data.TaskRepositoryImpl
 import kotlinx.coroutines.test.runBlockingTest
 import org.junit.Assert.assertEquals
 import org.junit.Before
@@ -12,17 +14,17 @@ class TaskRepositoryImplTest {
     private lateinit var repository: TaskRepository
 
     @Mock
-    private var taskDao:TaskDao
+    private var appDatabase:AppDatabase
 
     @Before
     fun setup() {
         MockitoAnnotations.initMocks(this)
-        repository = TaskRepositoryImpl(taskDao)
+        repository = TaskRepositoryImpl(appDatabase)
     }
     @Test
     fun testAddTask()=runBlockingTest{
         var task = Task(title="Title",description="Description")
-        'when'(taskDao.insert(task)).thenReturn(1)
+        'when'(appDatabase.taskDao().insertTask(task)).thenReturn(1)
         var  taskId = repository.addTask(task)
         assertEquals(1,taskId)
 
