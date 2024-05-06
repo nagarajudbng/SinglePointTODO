2221eaea891366d81bfac24bd80adc4a1da88606
nagarajudbng <nagaraj.dbng@gmail.com>
2024-05-07 03:10:43 +0530

    ViewModel TestCases Passed

--- app/src/main/java/com/single/point/feature_taskcreate/presentation/TodoViewModel.kt
+++ app/src/main/java/com/single/point/feature_taskcreate/presentation/TodoViewModel.kt
@@ -1,11 +1,16 @@
 package com.single.point.feature_taskcreate.presentation
 
 import androidx.lifecycle.ViewModel
+import com.single.point.core.data.database.Task
 import com.single.point.feature_taskcreate.domine.usecases.TaskUseCase
+import com.single.point.feature_taskcreate.presentation.util.TaskResult
 import javax.inject.Inject
 
 class TodoViewModel @Inject constructor(
     private var taskUseCase: TaskUseCase
 ):ViewModel(){
+    suspend fun insertTask(task: Task): TaskResult {
+     return  taskUseCase.insertTask(task)
+    }
 
 }
--- app/src/test/java/com/single/point/feature_taskcreate/presentation/TodoViewModelTest.kt
+++ app/src/test/java/com/single/point/feature_taskcreate/presentation/TodoViewModelTest.kt
@@ -3,13 +3,16 @@ package com.single.point.feature_taskcreate.presentation
 import com.single.point.core.data.database.Task
 import com.single.point.core.presentation.util.FieldStatus
 import com.single.point.feature_taskcreate.domine.usecases.TaskUseCase
+import com.single.point.feature_taskcreate.presentation.util.TaskResult
 import kotlinx.coroutines.test.runBlockingTest
 import org.junit.Assert
 import org.junit.Before
 import org.junit.Test
 import org.mockito.InjectMocks
 import org.mockito.Mock
+import org.mockito.Mockito.`when`
 import org.mockito.MockitoAnnotations
+import java.lang.reflect.Field
 
 // Created by Nagaraju Deshetty on 07/05/24.
 
@@ -29,13 +32,16 @@ class TodoViewModelTest {
     @Test
     fun insertTaskSuccess()= runBlockingTest{
        var task = Task(title="Title", description = "Description")
+        var taskResult = TaskResult(result = 1L)
+        `when`(taskUseCase.insertTask(task)).thenReturn(taskResult)
        var result = todoViewModel.insertTask(task)
-        Assert.assertEquals(FieldStatus.FieldFilled, result.title)
-        Assert.assertEquals(true, result.isValid)
+        Assert.assertEquals(taskResult, result)
     }
     @Test
     fun insertTaskError()= runBlockingTest{
-        var task = Task(title="Title", description = "Description")
+        var task = Task(title="Error", description = "Description")
+        var taskResult = TaskResult(isValid = false, title = FieldStatus.FieldEmpty)
+        `when`(taskUseCase.insertTask(task)).thenReturn(taskResult)
         var result = todoViewModel.insertTask(task)
         Assert.assertEquals(FieldStatus.FieldEmpty, result.title)
         Assert.assertEquals(false, result.isValid)
