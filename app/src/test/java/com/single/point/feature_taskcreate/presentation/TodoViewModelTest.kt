f461ec67057b5fee64e856200a7d91956a12a750
nagarajudbng <nagaraj.dbng@gmail.com>
2024-05-07 18:17:21 +0530

    Written viewModel Test case for get Tasklist but fails, need to write code until it is passed

--- app/src/test/java/com/single/point/feature_taskcreate/presentation/TodoViewModelTest.kt
+++ app/src/test/java/com/single/point/feature_taskcreate/presentation/TodoViewModelTest.kt
@@ -4,12 +4,16 @@ import com.single.point.core.data.database.Task
 import com.single.point.core.presentation.FieldStatus
 import com.single.point.feature_taskcreate.domine.usecases.TaskUseCase
 import com.single.point.feature_taskcreate.presentation.util.TaskResult
+import kotlinx.coroutines.flow.first
+import kotlinx.coroutines.flow.flowOf
+import kotlinx.coroutines.runBlocking
 import kotlinx.coroutines.test.runBlockingTest
 import org.junit.Assert
 import org.junit.Before
 import org.junit.Test
 import org.mockito.InjectMocks
 import org.mockito.Mock
+import org.mockito.Mockito
 import org.mockito.Mockito.`when`
 import org.mockito.MockitoAnnotations
 import java.lang.reflect.Field
@@ -45,5 +49,18 @@ class TodoViewModelTest {
         var result = todoViewModel.insertTask(task)
         Assert.assertEquals(FieldStatus.FieldEmpty, result.title)
         Assert.assertEquals(false, result.isValid)
+    }
+
+    @Test
+    fun getTaskList()= runBlocking {
+        var taskList = listOf(
+            Task(id = 1, title = "title 1", description = "Test Task"),
+            Task(id = 2, title = "title 2", description = "Test Task"),
+            Task(id = 3, title = "title 3", description = "Test Task")
+        )
+        `when` (taskUseCase.getTaskList()).thenReturn(flowOf(taskList))
+        var list = todoViewModel.getTaskList()
+        Mockito.verify(taskUseCase).getTaskList()
+        Assert.assertEquals(taskList, list.first())
     }
 }
